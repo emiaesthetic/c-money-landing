15d7f1c9fe2dd17ad15cdd38909198adbb3dea83
emiaesthetic <emi.aesthetic@gmail.com>
2025-03-27 08:52:32 +0500

    feat: add slider logic to feedback section

--- src/main.js
+++ src/main.js
@@ -1,9 +1,56 @@
 import 'virtual:svg-icons-register';
+import 'swiper/css';
 
 import initTabs from '@/components/tabs';
+import Swiper from 'swiper';
+import { Keyboard, Mousewheel, Navigation } from 'swiper/modules';
 
 import '@/assets/scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   initTabs();
+
+  new Swiper('.swiper', {
+    modules: [Keyboard, Navigation, Mousewheel],
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+        centeredSlides: true,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 23,
+        centeredSlides: false,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: -20,
+        centeredSlides: false,
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: false,
+      },
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+
+    mousewheel: {
+      releaseOnEdges: true,
+      invert: false,
+    },
+  });
 });
